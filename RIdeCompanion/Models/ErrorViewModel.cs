a476ba1688dfd32ed743e8a024f6a9986149c1cb
Artem Konkin <temakonkin@gmail.com>
2024-01-25 14:30:56 +0600

    Init

---
+++ RIdeCompanion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RIdeCompanion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

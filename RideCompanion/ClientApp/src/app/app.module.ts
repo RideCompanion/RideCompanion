222ce75eb92c2f763e084663af4feb91697f9708
KIN <temakonkin@gmail.com>
2024-04-07 16:58:03 +0600

    Add modules for driver home and companion

--- RideCompanion/ClientApp/src/app/app.module.ts
+++ RideCompanion/ClientApp/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from '../components/nav-menu/nav-menu.component';
+import { HomeComponent } from '../home/home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 

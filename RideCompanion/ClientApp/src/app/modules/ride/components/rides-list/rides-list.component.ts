e2f55c512523c8178e223db5d3d82efce3905c14
Artem Konkin <temakonkin@gmail.com>
2024-04-08 14:25:36 +0600

    Add ride module and components

---
+++ RideCompanion/ClientApp/src/app/modules/ride/components/rides-list/rides-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rides-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './rides-list.component.html',
+  styleUrl: './rides-list.component.scss'
+})
+export class RidesListComponent {
+
+}
